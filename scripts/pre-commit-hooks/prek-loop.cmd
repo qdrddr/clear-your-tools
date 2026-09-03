33dac677fa1121a98affe14a091755036fe762b5
Damien Berezenko <qdrddr@gmail.com>
2026-09-03 15:12:09 -0400

    chore: suppress pre-commit hook warnings in loop script
    - Added an environment variable `RTK_SUPPRESS_HOOK_WARNING=1` to the `prek-loop.cmd` script to suppress warnings during execution.
    - This change aims to improve the user experience by reducing unnecessary output while running pre-commit hooks.

--- scripts/pre-commit-hooks/prek-loop.cmd
+++ scripts/pre-commit-hooks/prek-loop.cmd
@@ -2,6 +2,7 @@
 :: Run `scripts\pre-commit-hooks\prek-loop.cmd --short --one-run` iteratively, do not omit or comment out issues, instead investigate root cause and fix the. Preserve the functionality.
 setlocal EnableExtensions
 
+set "RTK_SUPPRESS_HOOK_WARNING=1"
 set "BASH="
 for %%I in ("C:\Program Files\Git\bin\bash.exe" "C:\Program Files (x86)\Git\bin\bash.exe" "%ProgramFiles%\Git\bin\bash.exe") do (
 	if not defined BASH if exist %%~I set "BASH=%%~I"
